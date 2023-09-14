73a8c65904186ffd7ec18ddc1b4d6b4618c25afa
Felippe Santana <felippefsantana02@gmail.com>
2023-09-14 14:38:01 -0300

    integrationg with openai to transcription audio and send requests to gpt3.5-turbo-16k

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,25 @@
-import { fastify } from 'fastify';
-import { getAllPromptsRoute } from './routes/get-all-prompts';
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
 
 const app = fastify();
 
-app.register(getAllPromptsRoute);
-
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('HTTP server running');
+app.register(fastifyCors, {
+  origin: "*",
 });
+
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log("HTTP server running");
+  });
